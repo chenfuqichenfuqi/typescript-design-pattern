abcc832067e45225328b929f40e9371e210eb600
velpro <1326143109@qq.com>
2023-11-06 14:39:27 +0800

    ✨ feat:facade pattern bad smell

---
+++ facade/badSmell/App.ts
@@ -0,0 +1,20 @@
+import { DeliverySystem } from "./DeliverySystem";
+import { OrderSystem } from "./OrderSystem";
+import { PaymentSystem } from "./PaymentSystem";
+
+const main = () => {
+  const orderSystem = new OrderSystem();
+  const orderNumber = orderSystem.getOrderNum();
+
+  const paymentSystem = new PaymentSystem(orderSystem);
+  const orderAccount = paymentSystem.getOrderAccount();
+
+  const deliverySystem = new DeliverySystem();
+  const deliverytime = deliverySystem.getDeliverytime();
+
+  console.log(`orderNumber: ${orderNumber}`);
+  console.log(`orderAccount: ${orderAccount}`);
+  console.log(`deliverytime: ${deliverytime}`);
+};
+
+main();
---
+++ facade/badSmell/DeliverySystem.ts
@@ -0,0 +1,6 @@
+export class DeliverySystem {
+  public getDeliverytime(): number {
+    console.log("get delivery time");
+    return 30 * 60;
+  }
+}
